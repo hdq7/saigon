26f8b9a8b594f3d7133be8c3f75f4ffce159afb2
Hoang Doan <hoang@favesalon.com>
2024-12-03 22:00:42 +0700

    test

--- api/views.py
+++ api/views.py
@@ -3,4 +3,4 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def hello_world(request):
-    return Response({"message": "Hello, world!"})
+    return Response({"message": "Hello, Buddies!"})
